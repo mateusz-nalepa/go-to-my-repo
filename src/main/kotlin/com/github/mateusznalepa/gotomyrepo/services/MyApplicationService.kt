81dd3836ae96090f16e396337c716d2daef36f14
GitHub Action <action@github.com>
2021-04-17 21:55:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/mateusznalepa/gotomyrepo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mateusznalepa/gotomyrepo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mateusznalepa.gotomyrepo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mateusznalepa.gotomyrepo.MyBundle
 
 class MyApplicationService {
 
